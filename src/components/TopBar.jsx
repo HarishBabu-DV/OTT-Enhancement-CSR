ea1ed08a060f9c20e181c3dbb2d2adb1566621bc
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-25 19:42:05 +0530

    modified color for topbar again

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -38,7 +38,7 @@ const TopBar = () => {
         return ()=>document.removeEventListener('scroll',handleOnScroll)
     },[])
     return (
-        <header className={`${isScrolling ? `bg-[#ffffffe0] shadow-[0_3px_5px_#bababa] backdrop-blur-[7px] top-[5px] z-50` :`top-[15px] `}  transition-[top] duration-200 sticky rounded-md  bg-[#414141]`}>
+        <header className={`${isScrolling ? `bg-[#ffffffe0] shadow-[0_3px_5px_#bababa] backdrop-blur-[7px] top-[5px] z-50` :`top-[15px] `}  transition-[top] duration-200 sticky rounded-md  bg-[#252525]`}>
              <div className='flex flex-col w-full px-4 py-2'>
                 {/* Breadcrumb  */}
                 <p className='flex items-center gap-2 text-white'>
