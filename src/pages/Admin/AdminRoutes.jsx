580c4eb6b8aef103ee9228b141e044e9e98333d1
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-03-29 13:53:44 +0530

    modified patch in topbar

--- src/pages/Admin/AdminRoutes.jsx
+++ src/pages/Admin/AdminRoutes.jsx
@@ -23,7 +23,7 @@ const AdminRoutes = () => {
       <GlobalComponent>
         {/* SideBar  */}
         <SideBar  />
-        <div className='flex flex-col ml-[calc(200px+16px+16px)] mr-[16px] max-lg:mx-4'>
+        <div className='flex flex-col ml-[calc(200px+16px+16px)] mr-[16px] max-lg:mx-4 max-md:mx-0'>
           {/* TopBar  */}
           <TopBar />
           {/* Each Section  */}
