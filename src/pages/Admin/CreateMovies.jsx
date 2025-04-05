e4a4ae36ac29182d2fdb1ac338e94176610865d4
HarishBabu-DV <harishbabudv2002@gmail.com>
2025-04-05 18:50:19 +0530

    partially modified ui in createmovies page

--- src/pages/Admin/CreateMovies.jsx
+++ src/pages/Admin/CreateMovies.jsx
@@ -75,9 +75,15 @@ const CreateMovies = () => {
    }
 
   return (
-    <div className='w-full pl-[15%] flex flex-col gap-y-4'>
-        <h2 className='text-2xl font-semibold text-center'>Create New Movie</h2>
-        <form  className='ml-32 flex flex-col gap-y-6 w-3/4 p-10 ' onSubmit={handleSubmit}>
+    <section className='w-full pl-[15%] flex flex-col py-10 gap-y-4 bg-[#f7f7f7]'>
+        <form  className='bg-white ml-32 flex flex-col gap-y-6 w-3/4 p-10 ' onSubmit={handleSubmit}>
+            {/* Heading  */}
+            <h2 className='flex items-center'>
+                <span className='text-[35px]'> &#127916;</span>
+                <span className='text-2xl font-bold text-center signup-signin-heading'>Create Movies</span>
+            </h2>
+
+            <div className=' flex flex-col  gap-1 '>
             <div className='w-full flex'>
               <label className='block w-[25%] text-lg font-medium text-[#454545] ' htmlFor="name"> Name</label>
              
@@ -177,110 +183,12 @@ const CreateMovies = () => {
                     
                 }} />
             </div>
-
-            {/* Multi select 
-            <div className="flex flex-col w-[400px]  m-20" >
-                <input type="checkbox" name="vijay" id="vijay" value={"vijay"} onChange={(e)=>{
-                    console.log(e);
-                    
-                                if(e.target.checked){
-                                    actors.push(e.target.value)
-                                }
-                               else{
-                                    let indexNumber=actors.indexOf(e.target.value)
-                                    delete actors[indexNumber]
-                               }
-                            }} />
-                 <input type="checkbox" name="ajith" id="ajith" value={"ajith"} onChange={(e)=>{
-                                if(e.target.checked){
-                                    setDirectors([...directors,
-                                        e.target.value])
-                                    
-                                }
-                            }} />
-                  <input type="checkbox" name="vikram" id="vikram" value={"vikram"} onChange={(e)=>{
-                                if(e.target.checked){
-                                    setDirectors([...directors,
-                                        e.target.value])
-                                }
-                              
-                            }} />
-                <input type="checkbox" name="surya" id="surya" value={"surya"} onChange={(e)=>{
-                                if(e.target.checked){
-                                    setDirectors(
-                                        [...directors,
-                                        e.target.value])
-                                    
-                                }
-                            }} />
-                 <input type="checkbox" name="sk" id="sk" value={"sk"} onChange={(e)=>{
-                                if(e.target.checked){
-                                    setDirectors(
-                                        [...directors,
-                                        e.target.value])
-                                    
-                                }
-                            }} />
-
-
-              <div className='selected-directors-container flex items-center gap-x-4 flex-wrap border-1 border-gray-500 rounded-sm p-2 min-h-[30px]' onClick={handleSelectOnClick}>
-                
-                <div className='flex items-center gap-4 bg-gray-100 rounded-xl px-2 selected-vijay'>
-                    <span className='text-lg'>Vijay</span>
-                    <label htmlFor='vijay' className='text-lg hover:cursor-pointer'><IoCloseCircleSharp className='text-red-600' /></label>
-                </div>
-                <div className='flex items-center gap-4 bg-gray-100 rounded-xl px-2 selected-ajith'>
-                    <span className='text-lg'>Ajith</span>
-                    <label htmlFor='ajith' className='text-lg hover:cursor-pointer'><IoCloseCircleSharp className='text-red-600' /></label>
-                </div>
-                <div className='flex items-center gap-4 bg-gray-100 rounded-xl px-2 selected-vikram'>
-                    <span className='text-lg'>Vikram</span>
-                    <label htmlFor='vikram' className='text-lg hover:cursor-pointer'><IoCloseCircleSharp className='text-red-600' /></label>
-                </div>
-                <div className='flex items-center gap-4 bg-gray-100 rounded-xl px-2 selected-surya'>
-                    <span className='text-lg'>Surya</span>
-                    <label htmlFor='surya' className='text-lg hover:cursor-pointer'><IoCloseCircleSharp className='text-red-600' /></label>
-                </div>
-                <div className='flex items-center gap-4 bg-gray-100 rounded-xl px-2 selected-sk'>
-                    <span className='text-lg'>SK</span>
-                    <label htmlFor='sk' className='text-lg hover:cursor-pointer'><IoCloseCircleSharp className='text-red-600' /></label>
-                </div>
-                
-              </div>
-
-              {/* Select options  */}
-                {/* <div className={`w-full bg-[#f8f8f8] py-4 label-container`} >
-                    <ul>                           
-                       <label htmlFor='vijay' className='list-none p-4 flex justify-between items-center hover:bg-[#e8e8e8] '> 
-                            <span>Vijay</span>
-                            <span className='hidden vijay-label'><FaCheck /></span>
-                        </label>
-                       <label htmlFor='ajith' className='list-none p-4 flex justify-between items-center hover:bg-[#e8e8e8] '>
-                            <span>Ajith</span>
-                            <span className='hidden ajith-label'><FaCheck /></span>
-                        </label>
-                       <label htmlFor='vikram' className='list-none p-4 flex justify-between items-center hover:bg-[#e8e8e8]'>
-                            <span>Vikram</span>
-                            <span className='hidden vikram-label'><FaCheck /></span>
-                          
-                        </label>
-                       <label htmlFor='surya' className='list-none p-4 flex justify-between items-center hover:bg-[#e8e8e8]'>
-                            <span>Surya</span>
-                            <span className='hidden surya-label'><FaCheck /></span>
-                        </label>
-                       <label htmlFor='sk' className='list-none p-4 flex justify-between items-center hover:bg-[#e8e8e8]'>
-                            <span>SK</span>
-                            <span className='hidden sk-label'><FaCheck /></span>
-                        </label>
-                    </ul>
-                </div> 
-
-            </div> */}
             <div className=''>
                 <input type="submit" value="Create" className='bg-[#454545] w-1/4 hover:cursor-pointer text-white px-4 py-2 rounded-md'/>
             </div>
+            </div>
         </form>
-    </div>
+    </section>
   )
 }
 
